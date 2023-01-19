c266c14452f988096afe8ac91a4fe3764dd996de
Eugene015 <egavriluk@gmail.com>
2023-01-19 14:41:53 +0100

    items list works

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -79,9 +79,7 @@ function Item({ item, width }) {
       </Box>
       <Box mt="3px">
         <Typography variant="subtitle2" color={neutral.dark}>
-          {category
-            .replace(/([A-Z])/g, " $1")
-            .replace(/^./, (str) => str.toUpperCase())}
+          {category}
         </Typography>
         <Typography>{name}</Typography>
         <Typography fontWeight="bold">${price}</Typography>
