65f18ddfbbc8613c7edfbb2aef735bbb7ea90d06
Eugene015 <egavriluk@gmail.com>
2023-01-20 12:42:19 +0100

    item page added

--- src/scenes/home/itemDetails/ItemDetails.jsx
+++ src/scenes/home/itemDetails/ItemDetails.jsx
@@ -46,7 +46,102 @@ function ItemDetails() {
 
   return (
     <Box width="80%" m="80px auto">
-      <Box display="flex" flexWrap="wrap" columnGap="40px"></Box>
+      <Box display="flex" flexWrap="wrap" columnGap="40px">
+        {/* Pics */}
+        <Box flex="1 1 40%" mb="40px">
+          <img
+            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            style={{ objectFit: "contain" }}
+            alt={item?.name}
+            width="100%"
+            height="100%"
+          />
+        </Box>
+        {/* Actions */}
+        <Box flex="1 1 50%" mb="40px">
+          <Box display="flex" justifyContent="space-between">
+            <Box>Home/Item</Box>
+            <Box>Prev Next</Box>
+          </Box>
+          <Box m="65px 0 25px 0">
+            <Typography variant="h3">{item?.attributes?.name}</Typography>
+            <Typography>${item?.attributes?.price}</Typography>
+            <Typography sx={{ mt: "20px" }}>
+              {item?.attributes?.longDescription}
+            </Typography>
+          </Box>
+          {/* Count and Button */}
+          <Box display="flex" alignItems="center" minHeight="50px">
+            <Box
+              display="flex"
+              alignItems="center"
+              border={`1.5px solid ${shades.neutral[300]}`}
+              mr="20px"
+              p="2px 5px"
+            >
+              <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography sx={{ p: "0 5px" }}>{count}</Typography>
+              <IconButton onClick={() => setCount(count + 1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+            <Button
+              sx={{
+                backgroundColor: "#222222",
+                color: "white",
+                borderRadius: 0,
+                minWidth: "150px",
+                padding: "10px 40px",
+              }}
+              onClick={() => dispatch(addToCart({ item: { ...item, count } }))}
+            >
+              Add to Cart
+            </Button>
+          </Box>
+          <Box>
+            <Box m="20px 0 5px 0" display="flex">
+              <FavoriteBorderOutlinedIcon />
+              <Typography sx={{ ml: "5px" }}>Add to Wishlist</Typography>
+            </Box>
+            <Typography sx={{ ml: "5px" }}>
+              Categories: {item?.attributes?.category}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* Info */}
+      <Box m="20px 0">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Description" value="description" />
+          <Tab label="Reviews" value="reviews" />
+        </Tabs>
+      </Box>
+      <Box display="flex" flexWrap="wrap" gap="15px">
+        {value === "description" && (
+          <div>{item?.attributes?.longDescription}</div>
+        )}
+        {value === "reviews" && <div>reviews</div>}
+      </Box>
+      {/* Related Items */}
+      <Box mt="50px" width="100%">
+        <Typography variant="h3" fontWeight="bold">
+          Related Products
+        </Typography>
+        <Box
+          mt="20px"
+          display="flex"
+          flexWrap="wrap"
+          columnGap="1.33%"
+          justifyContent="space-between"
+        >
+          {items.slice(0, 4).map((item, i) => (
+            <Item key={`${item.name}-${i}`} item={item} />
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 }
