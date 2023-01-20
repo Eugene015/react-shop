fb7cec30830d2c3838085359200a4c6dd12228bf
Eugene015 <egavriluk@gmail.com>
2023-01-20 10:33:55 +0100

    items list added

---
+++ src/scenes/home/Sibscribe.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box, Typography, InputBase, Divider, IconButton } from "@mui/material";
+
+function Sibscribe() {
+  return <div>Sibscribe</div>;
+}
+
+export default Sibscribe;
